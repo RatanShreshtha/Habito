671028b868d3eec2735c416f04dab7d03a75bdad
Ratan Kulshreshtha <ratan.shreshtha@gmail.com>
2024-03-20 18:46:01 +0530

    Add supabase module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Habito',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          name: 'description',
+          content: 'A simple portal to track your habits and todo.'
+        }
+      ]
+    }
+  },
   css: ['@fortawesome/fontawesome-free/css/all.css'],
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
+  supabase: {
+    redirectOptions: {
+      login: '/login',
+      callback: '/confirm',
+      exclude: ['/*'],
+    },
+  },
 })
