ef97f64234a3ba6af193977d1982c7cdd64489ca
Ratan Kulshreshtha <ratan.shreshtha@gmail.com>
2024-03-21 18:19:27 +0530

    Add about, contact and license page

---
+++ pages/about.vue
@@ -0,0 +1,38 @@
+<template>
+    <main class="hero">
+        <div class="hero-content text-center">
+            <div class="max-w-2xl">
+                <h1 class="text-6xl font-bold">About</h1>
+                <p class="py-6">
+                    Welcome to Habito, your personal companion on the journey to building better habits and leading a
+                    more productive life.
+                </p>
+
+                <h2 class="text-4xl font-bold">Why Choose Habito?</h2>
+
+                <ul class="py-2">
+                    <li>
+                        Simple and Intuitive Design: Designed for ease of use.
+                    </li>
+                    <li>
+                        Customizable Tracking: Tailor the app to fit your lifestyle.
+                    </li>
+                    <li>
+                        Built-in Motivation: Includes features to keep you inspired and on track.
+                    </li>
+                    <li>
+                        Privacy and Security: Your data is encrypted and stored securely.
+                    </li>
+                </ul>
+
+                <p class="py-6">
+                    Thank you for choosing Habito to help you on your journey towards personal growth and self-improvement. We're excited to be a part of your success story!
+                </p>
+
+                <NuxtLink class="btn btn-neutral my-2" to="/">
+                    Take Me Home
+                </NuxtLink>
+            </div>
+        </div>
+    </main>
+</template>
